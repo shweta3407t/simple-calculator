ae6687f0a2b52bdd05beecfa05b38ea24805c8c8
Shweta Prajapati <shweta3407t@gmail.com>
2026-03-25 01:24:00 +0530

    handled  exception

--- InputValidation.java
+++ InputValidation.java
@@ -1,14 +1,11 @@
-package utils;
-
 public class InputValidation {
  
-    private static boolean isNumaric(int num){
+    public static boolean isNumaric( String num){
         if(num == null || num.isEmpty()){
             return false;
         }
     
         try{
-            Double.parseDouble(num);
             Integer.parseInt(num);
             return true;
         }catch(Exception e){
--- Main.java
+++ Main.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 
-
-
 public class Main {
 
     private static int add(int num1, int num2) {
@@ -31,75 +29,37 @@ public class Main {
 
     }
 
-    private static void calculator(Scanner sc) {
+    private static void calculator(String expression, int num1, int num2) {
 
-        while (true) {
+        switch (expression) {
+            case "1":
+                int addNum = add(num1, num2);
+                System.out.println("ADDITION  IS " + addNum);
 
-            System.out.println("""
-            PRESS 1 TO ADDITION
-            PRESS 2 TO SUBTRACTION
-            PRESS 3 TO MULTIPLY
-            PRESS 4 TO DIVIDE
-                    """);
+                break;
+            case "2":
+                int subNum = subtract(num1, num2);
 
-            System.out.print("ENTER CHOICE=");
-            String expression = sc.next();
-            if (expression.equals("1") || expression.equals( "2")  ||
-                    expression.equals("3" )|| expression.equals("4" ) ) {
-                System.out.print("ENTER FIRST NUMBER =");
-                int num1 = sc.nextInt();
+                System.out.println("SUBTRACTION  IS " + subNum);
 
-                System.out.print("ENTER SECOND NUMBER =");
-                int num2 = sc.nextInt();
+                break;
+            case "3":
+                int multiplyNum = multiply(num1, num2);
+                System.out.println("MULTIPLICATION  IS " + multiplyNum);
 
-                switch (expression) {
-                    case "1":
-                        int addNum = add(num1, num2);
-                        System.out.println("ADDITION  IS " + addNum);
+                break;
+            case "4":
+                if (num2 == 0) {
+                    System.out.println("....CANNOT DEVIDE BY ZERO....");
 
-                        break;
-                    case "2":
-                        int subNum = subtract(num1, num2);
-
-                        System.out.println("SUBTRACTION  IS " + subNum);
-
-                        break;
-                    case "3":
-                        int multiplyNum = multiply(num1, num2);
-                        System.out.println("MULTIPLICATION  IS " + multiplyNum);
-
-                        break;
-                    case "4":
-                        if (num2 == 0) {
-                            System.out.println("....CANNOT DEVIDE BY ZERO....");
-
-                        } else {
-                            double devideNum = devide(num1, num2) ;
-                            System.out.printf("DEVISION  IS  " + devideNum);
-                            System.out.println();
-                        }
-                        break;
-                    default:
-                        System.out.println("..........wrong operation selected...........");
-
-                }
-
-                System.out.println("ENTER e TO EXIT OR ENTER c TO CONTINUE.");
-                String s = sc.next();
-
-                if (s.equalsIgnoreCase("e")) {
-                    System.out.println("<<<<<<<<<<<<<EXITING PROGRAM.>>>>>>>>>>>>>>>>");
-                    break;
-                } else if (s.equalsIgnoreCase("c")) {
-                    System.out.println(".........CONTINUEING PROGRAM.............");
                 } else {
-                    System.out.println("INVALID INPUT.");
-                    break;
+                    double devideNum = devide(num1, num2);
+                    System.out.printf("DEVISION  IS  " + devideNum);
+                    System.out.println();
                 }
-
-            } else {
-                System.out.println("------WRONG CHOICE SELECTED------");
-            }
+                break;
+            default:
+                System.out.println("..........wrong operation selected...........");
 
         }
 
@@ -113,7 +73,36 @@ public class Main {
             boolean isRunning = true;
 
             while (isRunning) {
-                calculator(sc);
+                System.out.println("""
+                        PRESS 1 TO ADDITION
+                        PRESS 2 TO SUBTRACTION
+                        PRESS 3 TO MULTIPLY
+                        PRESS 4 TO DIVIDE
+
+                        PRESS C TO CONTINUE
+                        PRESS E TO EXIT
+                                """);
+
+                System.out.print("ENTER CHOICE=");
+                String expression = sc.next();
+
+                if (expression.equals("c")) {
+                    System.out.println(".......continuing program......");
+                    continue;
+                } else if (expression.equals("e")) {
+                    System.out.println("......exiting program......");
+                    isRunning = false;
+                    continue;
+
+                }
+
+                System.out.print("ENTER FIRST NUMBER =");
+                int num1 = sc.nextInt();
+
+                System.out.print("ENTER SECOND NUMBER =");
+                int num2 = sc.nextInt();
+
+                calculator(expression, num1, num2);
 
             }
 
